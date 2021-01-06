df78eadc8ebf23c353daff0d4e1d290cb7bc442d
devnido <luis@patina.la>
2021-01-05 23:48:32 -0300

    deperro

---
+++ index.js
@@ -0,0 +1,16 @@
+const getPokemons = require('./pokemons')
+
+const juegue = async() => {
+
+    const pokemons = await getPokemons()
+
+    /* escriba su codigo aqui */
+
+    // imprimir por consola todos los datos de pikachu
+    // https://pokeapi.co/api/v2/pokemon/25/ estructura de 1 pokemon
+
+    console.log(pokemons[25].name) // incorrecto
+
+}
+
+juegue()
